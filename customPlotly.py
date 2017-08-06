c499e5a604566eb32db89df380d69306ad73a356
jmahhh <hubertjoel@gmail.com>
2017-08-06 16:25:23 +0800

    multiprocessing + switched from plotly streaming to simple updating

--- customPlotly.py
+++ customPlotly.py
@@ -8,23 +8,28 @@ import time
 tls.set_credentials_file(username='jmahhh', api_key='5m6VBRHj52c314Qy8hTc', stream_ids=['j3mkjkmnp9'])
 tls.set_config_file(world_readable=True, sharing='public')
 
-stream_ids = tls.get_credentials_file()['stream_ids']
+# stream_ids = tls.get_credentials_file()['stream_ids']
 
 # Get stream id from stream id list
-stream_id = stream_ids[0]
+# stream_id = stream_ids[0]
 
-if sys.argv[0] == 'setup':
-    # Make instance of stream id object
-    stream_1 = go.Stream(
-        token=stream_id,  # link stream id to 'token' key
-        maxpoints=60      # keep a max of 60 pts on screen
-    )
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+def set_points(x, y, symbol, amount):
+    print('x: {0}'.format(x))
+    print('y: {0}'.format(y))
     trace1 = go.Scatter(
-        x=[],
-        y=[],
+        x=[x],
+        y=[y],
         mode='lines+markers',
-        stream=stream_1         # (!) embed stream id, 1 per trace
     )
 
     data = go.Data([trace1])
@@ -36,23 +41,51 @@ if sys.argv[0] == 'setup':
     fig = go.Figure(data=data, layout=layout)
 
     # Send fig to Plotly, initialize streaming plot, open new tab
-    py.plot(fig, filename='python-streaming')
+    py.plot(fig, fileopt='extend', filename='python-streaming', auto_open=False)
+    print(bcolors.FAIL + 'Sent to plotly ({0}, {1})\n'.format(symbol, amount) + bcolors.ENDC)
+
+if len(sys.argv) > 1 and sys.argv[1] == 'setup':
+    print('Entering plotly setup...')
+    trace1 = go.Scatter(
+        x=[],
+        y=[],
+        mode='lines+markers',
+    )
+
+    data = go.Data([trace1])
+
+    # Add title to layout object
+    layout = go.Layout(title='Time Series')
+
+    # Make a figure object
+    fig = go.Figure(data=data, layout=layout)
+
+    # Send fig to Plotly, initialize streaming plot, open new tab
+    py.plot(fig, fileopt='extend', filename='python-streaming')
+    # Make instance of stream id object
+    # stream_1 = go.Stream(
+    #     token=stream_id,  # link stream id to 'token' key
+    #     maxpoints=60      # keep a max of 60 pts on screen
+    # )
+
+
 
 # We will provide the stream link object the same token that's associated with the trace we wish to stream to
-s = py.Stream(stream_id)
+# s = py.Stream(stream_id)
 
 # We then open a connection
-s.open()
+# s.open()
+# print('Plotly stream opened...')
 
 # give time to open ~J
-time.sleep(5)
+# time.sleep(5)
 
 # (*) Import module keep track and format current time
-import datetime
-import time
+# import datetime
+# import time
 
-i = 0    # a counter
-k = 5    # some shape parameter
+# i = 0    # a counter
+# k = 5    # some shape parameter
 
 # this bit was moved to main.py ~J
 
--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import requests
 import json
 import configparser
 import sys
+import signal
 
-from customPlotly import s
+# should probably replace with threading
+from multiprocessing import Process, Queue
+from customPlotly import set_points
 from datetime import datetime
 
 from web3 import Web3, KeepAliveRPCProvider, IPCProvider, HTTPProvider
@@ -23,6 +26,28 @@ Config.read("config.ini")
 
 web3 = Web3(HTTPProvider(Config['Setup']['NodeURL']))
 
+# close plotly stream when SIGINT received
+def signal_handler(signal, frame):
+    # s.close()
+    # print(': Plotly stream closed... shutting down')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def reader(queue):
+    ## Read from the queue
+    while True:
+        msg = queue.get()
+        if (msg == 'DONE'):
+            break
+        # s.write(dict(x=msg[0], y=msg[1]))
+        set_points(msg[0], msg[1], msg[2], msg[3])
+
+def writer(array, queue):
+    ## Write to the queue
+    queue.put(array)   # Write 'count' numbers into the queue
+    queue.put('DONE')
+
 def new_block_callback(block_hash):
     b = web3.eth.getBlock(block_hash)
     print(bcolors.OKGREEN + "New Block: {0} | Number: {1}".format(block_hash, b['number']) + bcolors.ENDC + ' [' + str(datetime.now()) + ']')
@@ -31,45 +56,45 @@ def new_block_callback(block_hash):
         # first address(es) are token contracts
         # second address is hex-sha3 of ERC20 'Transfer' event
         # print(r['transactionHash'])
-        if (r['from'] in Config._sections['Exchanges'] or r['to'] in Config._sections['Exchanges']) and len(r['logs']) > 0 and r['logs'][0]['topics'][0] == Config['Setup']['TransferEvent']:
-            if r['from'] in Config._sections['Exchanges']:
-                action = 'from'
+        if r:
+            if (r['from'] in Config._sections['Exchanges'] or r['to'] in Config._sections['Exchanges']) and len(r['logs']) > 0 and r['logs'][0]['topics'][0] == Config['Setup']['TransferEvent']:
+                if r['from'] in Config._sections['Exchanges']:
+                    action = 'from'
+                else:
+                    action = 'to'
+                for ex in Config.items('Exchanges'):
+                    if ex[0] == r['from'] or ex[0] == r['to']:
+                        exchange = ex[1]
+                        break
+
+                print(bcolors.WARNING + 'Token transfer found! Exchange: {0} | Action: {1} | Block: {2}'.format(exchange, action, b['number'])+ bcolors.ENDC)
+                n = requests.get('https://api.ethplorer.io/getTokenInfo/'+r['logs'][0]['address']+'?apiKey=freekey')
+                n = json.loads(n.text)
+                decimals = int(n['decimals'])
+                amount = r['logs'][0]['data']
+                amount = str(int(amount, 16)) # hex to dec
+                keepIndex = len(amount) - decimals # adjust for decimals defined in token SC
+                amount = amount[0:keepIndex]+'.'+amount[keepIndex:-1]
+                print(bcolors.OKBLUE + 'Token: ' + bcolors.ENDC + '{0} ({1})\n Amount: '.format(n['name'], n['symbol']) + bcolors.BOLD + amount + bcolors.ENDC)
+
+                # send data to Plotly
+                if n['symbol'] == 'OMG':
+                    # Current time on x-axis, random numbers on y-axis
+                    x = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                    y = amount
+                    queue = Queue()
+                    reader_p = Process(target=reader, args=((queue),))
+                    reader_p.daemon = True
+                    reader_p.start()    # Launch reader() as a separate python process
+
+                    writer([x, y, n['symbol'], amount], queue)
+                    # reader_p.join() # Wait for the reader to finish
+
             else:
-                action = 'to'
-            for ex in Config.items('Exchanges'):
-                if ex[0] == r['from'] or ex[0] == r['to']:
-                    exchange = ex[1]
-                    break
-
-            print(bcolors.WARNING + 'Token transfer found! Exchange: {0} | Action: {1} | Block: {2}'.format(exchange, action, b['number'])+ bcolors.ENDC)
-            n = requests.get('https://api.ethplorer.io/getTokenInfo/'+r['logs'][0]['address']+'?apiKey=freekey')
-            n = json.loads(n.text)
-            decimals = int(n['decimals'])
-            amount = r['logs'][0]['data']
-            amount = str(int(amount, 16)) # hex to dec
-            keepIndex = len(amount) - decimals # adjust for decimals defined in token SC
-            amount = amount[0:keepIndex]+'.'+amount[keepIndex:-1]
-            print(bcolors.OKBLUE + 'Token: ' + bcolors.ENDC + '{0} ({1})\n Amount: '.format(n['name'], n['symbol']) + bcolors.BOLD + amount + bcolors.ENDC + '\n')
-
-            # send data to Plotly
-            if n['symbol'] == 'OMG':
-                # Current time on x-axis, random numbers on y-axis
-                x = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-                y = amount
-
-                # Send data to your plot
-                s.write(dict(x=x, y=y))
-                print(bcolors.FAIL + 'Sent to plotly\n' + bcolors.ENDC)
-
-                #     Write numbers to stream to append current data on plot,
-                #     write lists to overwrite existing data on plot
-
-                # Close the stream when done plotting
-                # s.close()
-
+                continue
+                # print('No etherdelta', r['from'], r['to'])
         else:
-            continue
-            # print('No etherdelta', r['from'], r['to'])
+            print('no r? {0}'.format(r))
 
 block_filter = web3.eth.filter('latest')
 print('Waiting for blocks...')
